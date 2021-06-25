946048b9c1c8f640989ef2154c3846b055206b9f
raaayden <46622202+raaayden@users.noreply.github.com>
2021-06-25 12:57:23 +0800

    6th update
    Customer portal already completed now 20% on plan page

--- db.sql
+++ db.sql
@@ -17,24 +17,25 @@ Plan_Name varchar(255),
 Type_Of_Plan varchar(255),
 Tariff varchar(30),
 Validity number(3),
-Rental number(3,2)
+Rental number(6,2)
 );
+
 
 -- Insert Relationship Manager, Operator and Admin
 INSERT INTO DispurUser
 (userID, userPWD, userGroup, Name, Address, Email, Contact_No, planID)
 VALUES
-(1001,"123","Relationship Manager","Amir","KL","amir_RM@dispur.com",123,1)
+(1001,'123','Relationship Manager','Amir','KL','amir_RM@dispur.com',123,1)
 
 INSERT INTO DispurUser
 (userID, userPWD, userGroup, Name, Address, Email, Contact_No, planID)
 VALUES
-(2001,"123","Operator","Anis","Singapore","anis_OR@dispur.com",9827332,1)
+(2001,'123','Operator','Anis','Singapore','anis_OR@dispur.com',9827332,1)
 
 INSERT INTO DispurUser
 (userID, userPWD, userGroup, Name, Address, Email, Contact_No, planID)
 VALUES
-(3001,"123","Admin","Ramu","India","ramu_admin@dispur.com",8827123,1)
+(3001,'123','Admin','Kumar','India','ramu_admin@dispur.com',8827123,1)
 
 
 -- Insert Plan none
@@ -43,9 +44,19 @@ INSERT INTO Plan
 VALUES 
 (1, 'NONE', 'NONE', '0/0', 0, 0.0);
 
+INSERT INTO Plan 
+(planID, Plan_Name, Type_Of_Plan, Tariff, Validity, Rental) 
+VALUES 
+(9125, 'Dispur5G', 'Data', '0.5/60', 90, 100.50);
+
 -- Select function
 select * from Plan left join DispurUser ON DispurUser.planID = Plan.planID;
 SELECT u.NAME, u.ADDRESS, u.EMAIL, u.CONTACT_NO, p.PLAN_NAME from DispurUser u LEFT JOIN Plan p ON p.planID = u.planID
 SELECT u.userID, u.NAME, u.EMAIL, u.CONTACT_NO, p.PLAN_NAME from DispurUser u LEFT JOIN Plan p ON p.planID = u.planID where USERGROUP = 'Customer'
 select * from Plan;
 select * from DispurUser;
+
+SELECT p.planID, p.plan_name, p.type_of_plan, p.tariff, p.validity, p.rental from Plan p LEFT JOIN DispurUser u ON u.planID = p.planID WHERE p.planID != (SELECT planID FROM DispurUser WHERE userID = 5405)
+
+DROP TABLE DispurUser;
+DROP TABLE Plan;
